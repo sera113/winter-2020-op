a3de668156b1726f0b35a7ca230d435c9eaed9b3
sera113 <selaph1122@gmail.com>
2026-06-20 20:37:38 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "party-rank-sorter",
-    title: "Party Rank Sorter",
+    localStoragePrefix: "winter-2020-op",
+    title: "winter-2020-op",
     description: "Party rank sorter for your custom list of songs."
 };
